92bda53e392a07df9cb48fd4aed31656f39ec4df
Boa Choi <165476198+swallowedB@users.noreply.github.com>
2026-03-08 21:16:24 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -0,0 +1,13 @@
+function solution(array) {
+    let max = array[0];
+    let index = 0;
+    
+    for (let i = 1; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+            index = i;
+        }
+    }
+    
+    return [max, index];
+}
